9c45e9093c67704f2f9627e26f4a5c9e5b778d21
amresh500 <164216845+amresh500@users.noreply.github.com>
2026-05-24 13:39:46 +0545

    Modify CORS middleware configuration
    Updated CORS middleware to allow all origins.

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const ALLOWED_ORIGINS = (
 connectDB();
 
 // Middleware
-app.use(cors({ origin: ALLOWED_ORIGINS, credentials: true }));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true, limit: "2mb" }));
